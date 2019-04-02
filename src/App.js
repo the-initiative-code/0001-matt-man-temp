c2bbb732fdba6ad1ee238d43681d298318c5fbb1
panzerstadt <tliqun@gmail.com>
2019-04-02 23:23:26 +0900

    temp website

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import logo from "./samurai-agency.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            alt="samurai agency"
+            className="samurai"
+            height={300}
+          />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
